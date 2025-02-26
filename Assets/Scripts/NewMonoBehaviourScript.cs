47abe618bb6aaa636f49495741b736001b2304ae
Haynes_Ethan <hayne382@hsd.k12.or.us>
2025-02-26 11:56:15 -0800

    finishing steps

--- Assets/Scripts/NewMonoBehaviourScript.cs
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -9,13 +9,16 @@ public class NewMonoBehaviourScript : MonoBehaviour
     }
 
     // Update is called once per frame
-public float turnSpeed;
-public float horizontalInput;
+private float speed = 20.00f;
+private float turnSpeed = 100.00f;
+private float horizontalInput;
+private float forwardInput;
     void Update()
     {
         //move the vehicle foward
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
